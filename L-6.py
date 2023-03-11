d1b4f70bd2bfc1636ddd47c5cdf30bb11297c317
Harutyun Madnatyan <madnatyan666@mail.ru>
2023-03-11 18:45:06 +0400

    2

--- L-6.py
+++ L-6.py
@@ -3,9 +3,19 @@
 # n = int(input(" n= "))
 # for i in range (1, n+1):
 #     for j in range(1, n+1):
-#         print(i, "*", j , "=", i*j)
+#         print(i, "*", j , "=", i*j, sep="")
 
 # tree count
 
-n = int(input(" n= "))
+# n = int(input(" n= "))
+# tiv = 1
+# tox = 1  
+# while tiv<=n:
+#     for i in range(tox):
+#         print(tiv, end=" ")
+#         tiv+=1
+#         if tiv > n:
+#             break
+#     tox+=1
+#     print()
 
