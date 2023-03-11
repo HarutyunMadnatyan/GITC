f565376f86b629cfc957f811f6e72cb7c445cf00
Harutyun Madnatyan <47569046+HarutyunMadnatyan@users.noreply.github.com>
2023-03-11 15:41:25 +0400

    1

---
+++ L-6.py
@@ -0,0 +1,11 @@
+#Table not like table
+
+# n = int(input(" n= "))
+# for i in range (1, n+1):
+#     for j in range(1, n+1):
+#         print(i, "*", j , "=", i*j)
+
+# tree count
+
+n = int(input(" n= "))
+
