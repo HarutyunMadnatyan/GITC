0f5b534b8031458e52c0d6a935c88b035a274b21
Harutyun Madnatyan <47569046+HarutyunMadnatyan@users.noreply.github.com>
2023-03-12 19:58:59 +0400

    4

--- L-6.py
+++ L-6.py
@@ -1,11 +1,11 @@
-#Table not like table
+# # Table not like table
 
 # n = int(input(" n= "))
 # for i in range (1, n+1):
 #     for j in range(1, n+1):
 #         print(i, "*", j , "=", i*j, sep="")
-
-# tree count
+# # 
+# # tree count
 
 # n = int(input(" n= "))
 # tiv = 1
@@ -18,4 +18,7 @@
 #             break
 #     tox+=1
 #     print()
+
+##Division table
+
 
--- L-7.py
+++ L-7.py
@@ -1,32 +1,55 @@
 
-n = int(input("qaxaqneri qanak - "))
-city = set()
+# n = int(input("qaxaqneri qanak - "))
+# city = set()
 
+# for i in range(n):
+#     c = True
+#     while c:
+#         anna = input("Anna asa qaxaqi anun - ")
+#         if anna in city:
+#             print("TRY ANOTHER")
+            
+#         else:
+#             city.add(anna)
+#             print("OK")
+#             c = False
+#     c = True       
+#     while c:
+#         natasha = input("Natasha asa qaxaqi anun - ")
+
+#         if natasha in city:
+#             print("TRY ANOTHER")
+            
+#         else:
+#             city.add(natasha)
+#             print("OK")
+#             c = False
+# print(city)
+
+
+
+
+
+# Books for summer
+
+m = int(input("Qani girq ka gradaranum? - "))
+n = int(input("Qani girq e handznararvac? - "))
+
+gradaran = set()
+for i in range(m):
+    gradaran_list = input("Mutqagrel gradarani grqeri anunner@ - ")
+    gradaran.add(gradaran_list)
+print(gradaran)
 for i in range(n):
-    c = True
-    while c:
-        anna = input("Anna asa qaxaqi anun - ")
-        if anna in city:
-            print("TRY ANOTHER")
-            
-        else:
-            city.add(anna)
-            print("OK")
-            c = False
-    c = True       
-    while c:
-        natasha = input("Natasha asa qaxaqi anun - ")
-
-        if natasha in city:
-            print("TRY ANOTHER")
-            
-        else:
-            city.add(natasha)
-            print("OK")
-            c = False
-print(city)
-
-autopep8 --in-place --aggressive L-7.py
+    amaran_grqer = input("Mutqagrel amran@ handznararvac greri anunner@ - ")
+    if amaran_grqer in gradaran:
+        print("YES")
+    else:
+        print("NO")
 
 
-    
+
+
+
+
+
