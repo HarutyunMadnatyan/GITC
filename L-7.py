60009576a4d6c08854066feedf615132824a42be
Harutyun Madnatyan <47569046+HarutyunMadnatyan@users.noreply.github.com>
2023-03-12 19:10:47 +0400

    1

---
+++ L-7.py
@@ -0,0 +1,31 @@
+
+n = int(input("qaxaqneri qanak - "))
+city = set()
+
+for i in range(n):
+    c = True
+    while c:
+        anna = input("Anna asa qaxaqi anun - ")
+        if anna in city:
+            print("TRY ANOTHER")
+            
+        else:
+            city.add(anna)
+            print("OK")
+            c = False
+    c = True       
+    while c:
+        natasha = input("Natasha asa qaxaqi anun - ")
+
+        if natasha in city:
+            print("TRY ANOTHER")
+            
+        else:
+            city.add(natasha)
+            print("OK")
+            c = False
+print(city)
+
+
+
+    
